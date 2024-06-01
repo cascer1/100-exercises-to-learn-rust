df12ab3e4b1264345e43988c78818a96d86f65ba
Cas Eliëns <git@cascer1.space>
2024-06-01 15:01:21 +0200

    Complete chapters 1 and 2

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,16 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 {
+        return 1;
+    }
+
+    let mut sum = 1;
+
+    for i in 1..=n {
+        sum *= i;
+    }
+
+    return sum;
 }
 
 #[cfg(test)]
